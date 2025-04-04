4efccb9246af2ccfc423ec1a507df4df583363c4
VNMihaylov22 <132439922+VNMihaylov22@users.noreply.github.com>
2025-04-04 13:56:57 +0300

    Add menus design

--- timeline/timeline/edit_menu.cpp
+++ timeline/timeline/edit_menu.cpp
@@ -1,13 +1,27 @@
+#include <fstream>
+#include <string>
 #include "../timeline_static_library/namespace_utility.h"
 #include <iostream>
 
 void edit_menu()
 {
+	std::ifstream file("assets/edit.txt");
+	if (!file) {
+		std::cerr << "Unable to open file edit.txt" << '\n';
+		return;
+	}
+
+	std::string line;
+	while (std::getline(file, line)) {
+		std::cout << line << '\n';
+	}
+	file.close();
+
 	int* edit_menu_screen = nullptr;
 	edit_menu_screen = new int;
 
-	std::cout << "1: Edit Event" << '\n';
-	std::cout << "2: Go Back" << '\n';
+	std::cout << "                              1: Edit Event" << '\n';
+	std::cout << "                              2: Go Back" << '\n';
 
 	while (true) {
 		if (std::cin >> *edit_menu_screen && (*edit_menu_screen == 1 || *edit_menu_screen == 2)) {
--- timeline/timeline/view_menu.cpp
+++ timeline/timeline/view_menu.cpp
@@ -1,33 +1,54 @@
 #include "../timeline_static_library/namespace_utility.h"
 #include <iostream>
+#include <fstream>
+#include <string>
 
 void view_menu()
 {
-	int* view_menu_screen = nullptr;
-	view_menu_screen = new int;
+	 
 
-	std::cout << "1: Select Event" << '\n';
-	std::cout << "2: Go Back" << '\n';
+    std::ifstream file("./assets/view.txt");
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            std::cout << line << '\n';
+        }
+        file.close();
+    }
+    else {
+        std::cout << "Unable to open file." << '\n';
+    }
 
-	while (true) {
-		if (std::cin >> *view_menu_screen && (*view_menu_screen == 1 || *view_menu_screen == 2)) {
-			break;
-		} else {
-			std::cout << "Invalid input." << '\n';
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		}
-	}
+    int* view_menu_screen = nullptr;
+    view_menu_screen = new int;
 
-	switch (*view_menu_screen)
-	{
-		case 1:
-			//Add functionality to select event
-			break;
+    std::cout << std::endl;
 
-		case 2:
-			delete view_menu_screen;
-			utility::scene.current_scene = utility::scene.menu;
-			break;
-	}
+    std::cout << "                              1: Select Event" << '\n';
+    std::cout << "                              2: Go Back" << '\n';
+
+    while (true) {
+        if (std::cin >> *view_menu_screen && (*view_menu_screen == 1 || *view_menu_screen == 2)) {
+            break;
+        } else {
+            std::cout << "Invalid input." << '\n';
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+
+    switch (*view_menu_screen)
+    {
+        case 1:
+            // Add functionality to select event
+            break;
+
+        case 2:
+            delete view_menu_screen;
+            utility::scene.current_scene = utility::scene.menu;
+            break;
+    }
+
+    
+   
 }
